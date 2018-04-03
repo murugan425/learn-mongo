5763ec530c8d9b50bc4f3c6f29b485b9e7908779
Murugan Nagarajan <murugan425@gmail.com>
2018-04-03 18:34:05 +0800

    Different way to import python libraries

--- pythonmongo/basics/hello_bottle.py
+++ pythonmongo/basics/hello_bottle.py
@@ -1,9 +1,27 @@
 #pip install bottle  
-from bottle import route, run, template
+#Option1: import only the required method
+#from bottle import route, run, template, debug
+
+#Option2: import all methods
+from bottle import *
+
+#Option3: import the whole library
+#import bottle
+
 
 @route('/hello/<name>')
 def index(name):
     return template('<b>Hai {{name}}</b>!', name=name)
 
-run(host='localhost', port=8082)
+#@bottle.route('/')
+@route('/')
+def home_page():
+	return "Hello, Welcome to Home Page, Murugan Nagarajan\n"
+	
+@route('/testpage')
+def test_page():
+	return "This is a Test Page for testing the connectivity"
 
+#@bottle.debug(True)	
+debug(True)
+run(host='localhost', port=8080)
