ef10dd096e870466b53454574afddb03fffda859
Murugan Nagarajan <murugan425@gmail.com>
2018-04-09 18:41:28 +0800

    Adding Projection to the Query

--- nodemongo/crud/finddata.js
+++ nodemongo/crud/finddata.js
@@ -9,9 +9,11 @@ MongoClient.connect('mongodb://localhost:27017/crunchbase', function(err, db) {
     assert.equal(err, null);
     console.log("Successfully connected to MongoDB.");
 
-    var query = {"category_code": "software"};
+    var filter = {"category_code": "software"};
+    var projection = {"name": 1, "category_code": 1, "_id": 0};
+
     //Query - Retrieve all the data and place it in a array object which takes memory
-    db.collection('companies').find(query).toArray(function(err, docs) {
+    /*db.collection('companies').find(query).toArray(function(err, docs) {
 
         assert.equal(err, null);
         assert.notEqual(docs.length, 0);
@@ -19,15 +21,24 @@ MongoClient.connect('mongodb://localhost:27017/crunchbase', function(err, db) {
         docs.forEach(function(doc) {
             console.log( doc.name + " is a " + doc.category_code + " company." );
         });
-    });
+    });*/
+
     //Cursor - Doesn't retrieve the data until the data is used.
     //Data will be retrieved as batches and the callback will be called for each batches.
     //So, the callback doesn't need to wait until all the data is retreived.
     //Callback Process is executed parallel to the DB Retreival, which improves the performance and reduces memory utilization
-    var cursor = db.collection('companies').find(query);
+    
+    //Append Projection to cursor
+    var cursor = db.collection('companies').find(filter);
+    cursor.project(projection);
+
+    
+    //var cursor = db.collection('companies').find(filter, projection);
+
     cursor.forEach(
         function(doc) {
-            console.log( doc.name + " is a " + doc.category_code + " company." );
+            //console.log( doc.name + " is a " + doc.category_code + " company." );
+            console.log(doc);
         },
         function(err) {
             assert.equal(err, null);
